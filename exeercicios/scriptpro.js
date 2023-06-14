9a0e4ed36cbfa71495e17f12e7307483f2393a65
PortadordeCNPJ <guilhermedesouzamuller@gmail.com>
2023-06-14 11:45:57 -0300

    att ultma

---
+++ exeercicios/scriptpro.js
@@ -0,0 +1,14 @@
+function logar() {
+    console.log(email)
+
+}
+
+let form = document.getElementById("prod");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if("submit" == onclick) {
+    location = "index.html"
+}
+});
